0bb9cd653ad8de8d7da726baf2c3f20fedf85ece
Avram Dames <avram.dames@gmail.com>
2023-10-19 15:24:58 +0300

    upgrade prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
