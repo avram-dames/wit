7ea97b65aa9474ece6917d57df4de17fd385b030
Avram Dames <avram.dames@gmail.com>
2023-10-24 08:50:54 +0300

    refactor members image location and filename

--- src/data/members.ts
+++ src/data/members.ts
@@ -2,28 +2,28 @@ import type { Member } from "./types";
 
 export const members: Member[] = [
     {
-      "imageSource": "Oana.webp",
+      "imageSource": "oana.webp",
       "name": "Oana Sipos",
       "ocupation": "Engineer & Educator",
       "twitterLink": "https://twitter.com/oanasipos",
       "linkedinLink": "https://www.linkedin.com/in/oanasipos/"
     },
     {
-      "imageSource": "Mela.webp",
+      "imageSource": "mela.webp",
       "name": "Melania Moldovan",
       "ocupation": "Software Engineer",
       "twitterLink": "https://twitter.com/mela_moldovan",
       "linkedinLink": "https://www.linkedin.com/in/melania-moldovan-b71083158/"
     },
     {
-      "imageSource": "Gina.webp",
+      "imageSource": "gina.webp",
       "name": "Georgina Lupu-Florian",
       "ocupation": "Engineer & Entrepreneur",
       "twitterLink": "https://twitter.com/ginalupu?lang=en",
       "linkedinLink": "https://www.linkedin.com/in/georginalupu/"
     },
     {
-      "imageSource": "Ioana.webp",
+      "imageSource": "ioana.webp",
       "name": "Ioana Chiorean",
       "ocupation": "Engineer & Manager",
       "twitterLink": "https://twitter.com/ioana_cis",
